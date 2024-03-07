ad3980b09f788eb9ace8a454e4a96d24f95b7865
PurpleNurps <seanlunt1991@gmail.com>
2024-03-07 11:42:13 +0000

    annalyns-infiltration complete

--- javascript/annalyns-infiltration/annalyns-infiltration.js
+++ javascript/annalyns-infiltration/annalyns-infiltration.js
@@ -27,7 +27,7 @@
  * @return {boolean} Whether or not you can execute a fast attack.
  */
 export function canExecuteFastAttack(knightIsAwake) {
-  return knightIsAwake ? false : true;
+  return !knightIsAwake;
 }
 
 /**
@@ -40,7 +40,7 @@ export function canExecuteFastAttack(knightIsAwake) {
  * @returns {boolean} Whether or not you can spy on someone.
  */
 export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
-  return knightIsAwake || archerIsAwake || prisonerIsAwake ? true : false;
+  return knightIsAwake || archerIsAwake || prisonerIsAwake;
 }
 
 /**
@@ -52,7 +52,7 @@ export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
  * @returns {boolean} Whether or not you can send a signal to the prisoner.
  */
 export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
-  return prisonerIsAwake && !archerIsAwake ? true : false;
+  return prisonerIsAwake && !archerIsAwake;
 }
 
 /**
@@ -71,5 +71,5 @@ export function canFreePrisoner(
   prisonerIsAwake,
   petDogIsPresent,
 ) {
-  return (petDogIsPresent && !archerIsAwake) || (prisonerIsAwake && !knightIsAwake && !archerIsAwake) ? true : false; 
+  return (petDogIsPresent && !archerIsAwake) || (prisonerIsAwake && !knightIsAwake && !archerIsAwake); 
 }
